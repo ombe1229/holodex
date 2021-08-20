978e12af81b84bd065d6b97524609da2ab33a0b4
ombe <h3236516@gmail.com>
2021-08-21 03:22:24 +0900

    style(client): add docstring

--- holodex/client.py
+++ holodex/client.py
@@ -6,8 +6,23 @@ from holodex.model.channel_info import ChannelInfo
 
 
 class HolodexClient(HolodexHttpClient):
+    """
+    Holodex client
+    """
+
     def __init__(self, session: Optional[ClientSession] = None) -> None:
         super().__init__(session)
 
     async def channel_info(self, channel_id: str) -> ChannelInfo:
+        """
+        ## channel_info
+        Get channel info by channel id
+
+        ### Parameters
+         - `channel_id` (str): channel id
+
+        ### Returns
+         - Class `ChannelInfo`
+
+        """
         return ChannelInfo(await self.get_channel(channel_id))
