6d1dd63a9dfa65f231b5896d73faed91ba6b2b45
ombe <h3236516@gmail.com>
2021-08-22 01:55:23 +0900

    feat(client): add add autocomplete

--- holodex/client.py
+++ holodex/client.py
@@ -1,3 +1,4 @@
+from holodex.model.autocomplete import AutoComplete
 from typing import Optional
 from aiohttp.client import ClientSession
 
@@ -6,9 +7,11 @@ from holodex.model.channel_info import ChannelInfo
 
 
 class HolodexClient(HolodexHttpClient):
-
     def __init__(self, session: Optional[ClientSession] = None) -> None:
         super().__init__(session)
 
     async def channel_info(self, channel_id: str) -> ChannelInfo:
         return ChannelInfo(await self.channels(channel_id))
+
+    async def autocomplete(self, keyword: str) -> AutoComplete:
+        return AutoComplete(await self.search_autocomplete(keyword))
