414626d81f1e27c995c642ad440419e38b13ee44
ombe <h3236516@gmail.com>
2021-08-23 17:45:54 +0900

    refactor(channel): Optional

--- holodex/model/channel.py
+++ holodex/model/channel.py
@@ -1,7 +1,7 @@
-from typing import Any, List, Literal, Optional
+from typing import Any, Literal, Optional
 
 
-class ChannelInfo:
+class Channel:
     def __init__(self, response: Any) -> None:
         self._response = response
 
@@ -15,88 +15,88 @@ class ChannelInfo:
 
     @property
     def english_name(self) -> Optional[str]:
-        return self._response["english_name"]
+        return self._response.get("english_name")
 
     @property
-    def description(self) -> str:
-        return self._response["description"]
+    def description(self) -> Optional[str]:
+        return self._response.get("description")
 
     @property
     def photo(self) -> Optional[str]:
         return self._response["photo"]
 
     @property
-    def thumbnail(self) -> str:
-        return self._response["thumbnail"]
+    def thumbnail(self) -> Optional[str]:
+        return self._response.get("thumbnail")
 
     @property
     def banner(self) -> Optional[str]:
-        return self._response["banner"]
+        return self._response.get("banner")
 
     @property
     def org(self) -> Optional[str]:
-        return self._response["org"]
+        return self._response.get("org")
 
     @property
     def suborg(self) -> Optional[str]:
-        return self._response["suborg"]
+        return self._response.get("suborg")
 
     @property
     def lang(self) -> Optional[str]:
-        return self._response["lang"]
+        return self._response.get("lang")
 
     @property
-    def published_at(self) -> str:
-        return self._response["published_at"]
+    def published_at(self) -> Optional[str]:
+        return self._response.get("published_at")
 
     @property
     def view_count(self) -> Optional[str]:
-        return self._response["view_count"]
+        return self._response.get("view_count")
 
     @property
     def video_count(self) -> Optional[str]:
-        return self._response["video_count"]
+        return self._response.get("video_count")
 
     @property
     def subscriber_count(self) -> Optional[str]:
-        return self._response["subscriber_count"]
+        return self._response.get("subscriber_count")
 
     @property
-    def comments_crawled_at(self) -> str:
-        return self._response["comments_crawled_at"]
+    def comments_crawled_at(self) -> Optional[str]:
+        return self._response.get("comments_crawled_at")
 
     @property
-    def updated_at(self) -> str:
-        return self._response["updated_at"]
+    def updated_at(self) -> Optional[str]:
+        return self._response.get("updated_at")
 
     @property
-    def yt_uploads_id(self) -> str:
-        return self._response["yt_uploads_id"]
+    def yt_uploads_id(self) -> Optional[str]:
+        return self._response.get("yt_uploads_id")
 
     @property
-    def crawled_at(self) -> str:
-        return self._response["crawled_at"]
+    def crawled_at(self) -> Optional[str]:
+        return self._response.get("crawled_at")
 
     @property
-    def type(self) -> Literal["vtuber", "subber"]:
-        return self._response["type"]
+    def type(self) -> Optional[Literal["vtuber", "subber"]]:
+        return self._response.get("type")
 
     @property
     def clip_count(self) -> Optional[int]:
-        return self._response["clip_count"]
+        return self._response.get("clip_count")
 
     @property
     def twitter(self) -> Optional[str]:
-        return self._response["twitter"]
+        return self._response.get("twitter")
 
     @property
-    def inactive(self) -> bool:
-        return self._response["inactive"]
+    def inactive(self) -> Optional[bool]:
+        return self._response.get("inactive")
 
     @property
-    def created_at(self) -> str:
-        return self._response["created_at"]
+    def created_at(self) -> Optional[str]:
+        return self._response.get("created_at")
 
     @property
-    def top_topics(self) -> List[str]:
-        return self._response["top_topics"]
+    def top_topics(self) -> Optional[list[str]]:
+        return self._response.get("top_topics")
