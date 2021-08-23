f8b2bcb2daa6c140d4aa9135ccd5dbbed7cdd39a
Ryu JuHeon <saidbysolo@gmail.com>
2021-08-23 09:30:07 +0000

    test(tests): add conftest and refactor test code

--- tests/test_autocomplete.py
+++ tests/test_autocomplete.py
@@ -1,9 +1,6 @@
 import pytest
-from holodex.client import HolodexClient
-
 
 @pytest.mark.asyncio
-async def test_autocomplete():
-    async with HolodexClient() as client:
-        channel = await client.autocomplete("gawr gura ch")
-        assert channel.contents[0].text == "Gawr Gura Ch. hololive-EN"
+async def test_autocomplete(client):
+    channel = await client.autocomplete("gawr gura ch")
+    assert channel.contents[0].text == "Gawr Gura Ch. hololive-EN"
