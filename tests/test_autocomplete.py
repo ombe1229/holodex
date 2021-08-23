84fd74582a9ae9b1d1bbcab0ad524a2c1a6986ff
ombe <h3236516@gmail.com>
2021-08-23 18:01:17 +0900

    test(autocomplete): change test code

--- tests/test_autocomplete.py
+++ tests/test_autocomplete.py
@@ -5,5 +5,5 @@ from holodex.client import HolodexClient
 @pytest.mark.asyncio
 async def test_autocomplete():
     async with HolodexClient() as client:
-        channel = await client.autocomplete("gura")
+        channel = await client.autocomplete("gawr gura ch")
         assert channel.contents[0].text == "Gawr Gura Ch. hololive-EN"
